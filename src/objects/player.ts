185f0e238fb54970084f41f9e1acbae55ce064c5
Thom Mott <thommott@proton.me>
2025-02-09 12:14:17 -0800

    Smoothing lerp & temp disabling forward only movement

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -8,7 +8,7 @@ const { sin, cos, tan, asin, acos, atan, PI } = Math;
 
 export class Player extends Physics.Arcade.Sprite {
     // body is reserved by Sprite
-    private drag : number = 0.99;
+    private drag : number = 0.9;
     private terminalVelocity : number = 400;
     private glideSpeed = 0.1;
 
@@ -23,7 +23,7 @@ export class Player extends Physics.Arcade.Sprite {
 
         this.body.setSize(this.width, this.height);
         //this.setDamping(true);
-        //this.setDrag(this.drag);
+        this.setDrag(this.drag);
         this.setMaxVelocity(this.terminalVelocity);
         this.setVelocityX(100);
     }
@@ -43,14 +43,14 @@ export class Player extends Physics.Arcade.Sprite {
         var pos : pMath.Vector2 = new pMath.Vector2(this.scene.input.activePointer.worldX, this.scene.input.activePointer.worldY).subtract(this.body.position);
         //console.log(`rPos: <${Math.round(rawPos.x * 100) / 100}, ${Math.round(rawPos.y * 100) / 100}>  Pos: <${Math.round(pos.x * 100) / 100}, ${Math.round(pos.y * 100) / 100}>`);
         pos = pos.normalize();
-        pos.x = pMath.Clamp(pos.x, 0, 1);
+        //pos.x = pMath.Clamp(pos.x, 0, 1);
         //console.log(`<${pos.x}, ${pos.y}>`);
 
         let pitch : number = pMath.Clamp(asin(pos.y), -PI * 0.5, PI * 0.5);
         this.rotation = pitch;
 
 
-        this.glide3(delta, pitch, pos);
+        this.glide(delta, pitch, pos);
 
         // temp wrap around for testing
         if(1000 < this.x) {
@@ -61,8 +61,8 @@ export class Player extends Physics.Arcade.Sprite {
         }
     }
 
-    private glide3(delta : number, pitch : number, dir : pMath.Vector2) : void {
-        let vel : pMath.Vector2 = this.body.velocity; 
+    private glide(delta : number, pitch : number, dir : pMath.Vector2) : void {
+        let vel : pMath.Vector2 = this.body.velocity;
         // you need to counteract gravity sin(pitch) needs to be added to your y compontent in the vel vector this
         // should be greater than gravity to move you up if pi/2 > pitch > 0 and move you down.
         // looking down all the way would be all of gravity taking you down and based on the angle you
@@ -70,8 +70,9 @@ export class Player extends Physics.Arcade.Sprite {
         let speedUp : number = (2 * pitch)/PI;
         let mag  : number = vel.length();
         let dot  : number = vel.normalize().dot(dir);
+        let smoothDir : pMath.Vector2 = vel.normalize().lerp(dir, 0.2);
 
-        let nDir = dir.scale(mag + (speedUp * 4));
+        let nDir = smoothDir.scale(mag + (speedUp * 4));
         this.setVelocity(nDir.x, nDir.y);
         console.log(`SpeedUp: ${Math.round(speedUp * 1000)/1000}  dot: ${Math.round(dot*1000)/1000}   mag ${Math.round(mag*1000)/1000}   nDir: <${Math.round(nDir.x*1000)/1000}, ${Math.round(nDir.y*1000)/1000}>`);
     }
